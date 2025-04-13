0a757c38149e751a1feabc245eaafb79121765e0
Kazuya H <kazuya@yx2.com>
2025-04-13 16:15:36 -0700

    Add files via upload
    Code up till Triangles, should be placing Triangles now

--- asgn1/ColoredPoints.js
+++ asgn1/ColoredPoints.js
@@ -29,7 +29,8 @@ function setupWebGL(){
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  //gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true})
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -96,6 +97,8 @@ function main() {
   
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
+  //canvas.onmousemove = click;
+  canvas.onmousemove = function (ev)  { if(ev.buttons == 1) {click(ev)}};
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -142,6 +145,8 @@ function click(ev) {
 }
 
 function renderAllShapes() {
+  var startTime = performance.now();
+  
     // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -150,4 +155,17 @@ function renderAllShapes() {
     g_shapesList[i].render();
     
   }
+
+  var duration = performance.now() - startTime;
+  //debug time
+  sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(1000/duration)/10, "numdot");
+  }
+
+function sendTextToHTML(text, htmlID) {
+  var htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
 }
